208fe827cb8ed9f34aba9fe55c7c8a1176506e31
gucerbuq <gbugrayalcin@gmail.com>
2023-01-31 14:54:58 +0200

    RestApi and Exceptions are updated #1

--- auth-service/src/main/java/com/bilgeadam/group1/constants/RestApi.java
+++ auth-service/src/main/java/com/bilgeadam/group1/constants/RestApi.java
@@ -1,4 +1,20 @@
 package com.bilgeadam.group1.constants;
 
 public class RestApi {
+
+    public static final String VERSION="api/v1";
+    public static final String AUTH=VERSION+"/auth";
+
+    public static final String REGISTERSITEMANAGER="/registersitemanager";
+    public static final String REGISTERGENERALMANAGER="/registergeneralmanager";
+    public static final String LOGIN="/login";
+    public static final String UPDATE="/update";
+    public static final String DELETEBYID="/deletebyid";
+    public static final String DELETEBYAUTHID="/deletebyauthid";
+    public static final String CREATE="/create";
+    public static final String UPDATEBYEMAIL="/updateusernameoremail";
+    public static final String FINDALL="/findall";
+    public static final String FINDBYID="/findbyid";
+    public static final String GETBYROLE="/getbyrole/{role}";
+
 }
