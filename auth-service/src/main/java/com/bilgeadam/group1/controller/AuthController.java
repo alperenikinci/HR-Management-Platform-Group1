b95ff1060218ebaa17243f193e5977a8380907fc
akkasezgi <ezgituncalp@hotmail.com>
2023-01-31 15:58:27 +0300

    31.01.2023 / login request-response / controller

--- auth-service/src/main/java/com/bilgeadam/group1/controller/AuthController.java
+++ auth-service/src/main/java/com/bilgeadam/group1/controller/AuthController.java
@@ -1,4 +1,46 @@
 package com.bilgeadam.group1.controller;
 
+
+import com.bilgeadam.group1.dto.request.LoginRequestDto;
+import com.bilgeadam.group1.dto.request.RegisterRequestDto;
+import com.bilgeadam.group1.dto.response.LoginResponseDto;
+import com.bilgeadam.group1.dto.response.RegisterResponseDto;
+import com.bilgeadam.group1.service.WebsiteManagerService;
+import com.bilgeadam.group1.utility.JwtTokenManager;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static com.bilgeadam.group1.constants.RestApi.*;
+@RestController
+@RequestMapping(AUTH)
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final WebsiteManagerService websiteManagerService;
+    private final JwtTokenManager jwtTokenManager;
+
+
+    @PostMapping(REGISTERSITEMANAGER)
+    @Operation(summary = "Website manager kayit eden metot")
+    public ResponseEntity<RegisterResponseDto> registerWebsiteManager(@RequestBody @Valid RegisterRequestDto dto){
+        return  ResponseEntity.ok( websiteManagerService.register(dto));
+    }
+
+    @PostMapping(REGISTERGENERALMANAGER)
+    @Operation(summary = "General Manager kayit eden metot")
+    public ResponseEntity<RegisterResponseDto> registerGeneralManager(@RequestBody @Valid RegisterRequestDto dto){
+        return  ResponseEntity.ok( websiteManagerService.register(dto));
+    }
+
+    @PostMapping(LOGIN)
+    public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto dto){
+        return  ResponseEntity.ok(websiteManagerService.login(dto));
+    }
 }
---
+++ auth-service/src/main/java/com/bilgeadam/group1/dto/response/LoginResponseDto.java
@@ -0,0 +1,17 @@
+package com.bilgeadam.group1.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponseDto {
+
+    private String email;
+    private String password;
+    private String token;
+}
