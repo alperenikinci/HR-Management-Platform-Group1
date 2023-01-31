adb408f158aa5912c76e3aa96f2e72c09e94682e
akkasezgi <ezgituncalp@hotmail.com>
2023-01-31 15:46:24 +0300

    31.01.2023 / register request-response

---
+++ auth-service/src/main/java/com/bilgeadam/group1/dto/request/RegisterRequestDto.java
@@ -0,0 +1,32 @@
+package com.bilgeadam.group1.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequestDto {
+
+    @Email(message = "Lutfen gecerli bir e-mail adresi giriniz.")
+    @NotBlank(message = "Kullanici e-maili bos gecilemez.")
+    private String email;
+
+    @NotBlank(message = "Şifre boş geçilemez")
+    @Size(min = 8,max = 16)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=*!])(?=\\S+$).{8,}$")
+    private String password;
+
+    @NotBlank(message = "Şifre boş geçilemez")
+    @Size(min = 8,max = 16)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=*!])(?=\\S+$).{8,}$")
+    private String repassword;
+}
--- auth-service/src/main/java/com/bilgeadam/group1/dto/response/res.java
+++
@@ -1,4 +0,0 @@
-package com.bilgeadam.group1.dto.response;
-
-public class res {
-}
