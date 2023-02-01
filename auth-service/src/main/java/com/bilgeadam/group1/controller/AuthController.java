d7460b93c02289fcd2df127f703577477fd26df2
akkasezgi <ezgituncalp@hotmail.com>
2023-02-01 14:41:08 +0300

    31.01.2023 / findall

--- auth-service/src/main/java/com/bilgeadam/group1/controller/AuthController.java
+++ auth-service/src/main/java/com/bilgeadam/group1/controller/AuthController.java
@@ -5,17 +5,17 @@ import com.bilgeadam.group1.dto.request.LoginRequestDto;
 import com.bilgeadam.group1.dto.request.RegisterRequestDto;
 import com.bilgeadam.group1.dto.response.LoginResponseDto;
 import com.bilgeadam.group1.dto.response.RegisterResponseDto;
+import com.bilgeadam.group1.repository.entity.WebsiteManager;
 import com.bilgeadam.group1.service.WebsiteManagerService;
 import com.bilgeadam.group1.utility.JwtTokenManager;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.List;
 
 import static com.bilgeadam.group1.constants.RestApi.*;
 @RestController
@@ -29,18 +29,27 @@ public class AuthController {
 
     @PostMapping(REGISTERWEBSITEMANAGER)
     @Operation(summary = "Website manager kayit eden metot")
-    public ResponseEntity<RegisterResponseDto> registerWebsiteManager(@RequestBody @Valid RegisterRequestDto dto){
-        return  ResponseEntity.ok(websiteManagerService.registerWebsiteManager(dto));
+    public ResponseEntity<RegisterResponseDto> registerWebsiteManager(@RequestBody @Valid RegisterRequestDto dto) {
+        return ResponseEntity.ok(websiteManagerService.registerWebsiteManager(dto));
     }
 
     @PostMapping(REGISTERGENERALMANAGER)
     @Operation(summary = "General Manager kayit eden metot")
-    public ResponseEntity<RegisterResponseDto> registerGeneralManager(@RequestBody @Valid RegisterRequestDto dto){
-        return  ResponseEntity.ok( websiteManagerService.registerWebsiteManager(dto));
+    public ResponseEntity<RegisterResponseDto> registerGeneralManager(@RequestBody @Valid RegisterRequestDto dto) {
+        return ResponseEntity.ok(websiteManagerService.registerWebsiteManager(dto));
     }
 
 //    @PostMapping(LOGIN)
 //    public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto dto){
 //        return  ResponseEntity.ok(websiteManagerService.login(dto));
 //    }
+
+
+    @GetMapping(FINDALL)
+    public ResponseEntity<List<WebsiteManager>> findAll(){
+        return ResponseEntity.ok(websiteManagerService.findAll());
+    }
+
+
+
 }
