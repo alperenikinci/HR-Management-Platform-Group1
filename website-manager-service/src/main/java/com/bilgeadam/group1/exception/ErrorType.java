794bd10b3e792781979ebf5fd1c938f95b18827f
gucerbuq <gbugrayalcin@gmail.com>
2023-02-03 09:59:44 +0200

    WebsiteManager service exceptions are updated. #2

--- website-manager-service/src/main/java/com/bilgeadam/group1/exception/ErrorType.java
+++ website-manager-service/src/main/java/com/bilgeadam/group1/exception/ErrorType.java
@@ -13,8 +13,8 @@ public enum ErrorType {
 
     INTERNAL_ERROR(5100,"Sunucu Hatasi", HttpStatus.INTERNAL_SERVER_ERROR),
     BAD_REQUEST(2100,"Parametre Hatasi",HttpStatus.BAD_REQUEST),
-    EMAIL_NOT_FOUND(2111,"Bu email'e ait bir kullanici bulunamadi.",HttpStatus.BAD_REQUEST),
-    WEBSITE_MANAGER_NOT_CREATED(2113,"WebsiteManager Olusturulamadi",HttpStatus.BAD_REQUEST),
+    EMAIL_NOT_FOUND(2117,"Bu email'e ait bir kullanici bulunamadi.",HttpStatus.BAD_REQUEST),
+    WEBSITE_MANAGER_NOT_CREATED(2118,"WebsiteManager Olusturulamadi",HttpStatus.BAD_REQUEST),
     INVALID_TOKEN(2114,"Gecersiz Token?",HttpStatus.BAD_REQUEST);
     //TODO hata açıklamasına bak.
     private int code;
