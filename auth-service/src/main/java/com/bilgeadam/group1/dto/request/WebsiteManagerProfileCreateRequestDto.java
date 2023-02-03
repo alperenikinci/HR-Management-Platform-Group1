4b3b961b6999bcd579b2969b99b76feb12cd6d6f
akkasezgi <ezgituncalp@hotmail.com>
2023-02-03 14:15:16 +0300

    03.02.2023 / valid

--- auth-service/src/main/java/com/bilgeadam/group1/dto/request/WebsiteManagerProfileCreateRequestDto.java
+++ auth-service/src/main/java/com/bilgeadam/group1/dto/request/WebsiteManagerProfileCreateRequestDto.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -13,6 +16,11 @@ import lombok.NoArgsConstructor;
 public class WebsiteManagerProfileCreateRequestDto {
 
     private Long websiteManagerId;
+
+    @Email(message = "Lufen e-mail formatinda bir deger giriniz.")
+    @NotBlank(message = "E-mail bos gecilemez. ")
     private String email;
+
+    @NotBlank(message = "Token bilgisi bos gecilemez. Lutfen token bilgisi giriniz.")
     private String token;
 }
--- website-manager-service/src/main/java/com/bilgeadam/group1/dto/request/WebsiteManagerProfileCreateRequestDto.java
+++ website-manager-service/src/main/java/com/bilgeadam/group1/dto/request/WebsiteManagerProfileCreateRequestDto.java
@@ -6,13 +6,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class WebsiteManagerProfileCreateRequestDto {
 
+    @NotBlank(message = "id bilgisi bos gecilemez. ")
     private Long websiteManagerId;
+
+    @Email(message = "Lutfen e-mail formatinda bir deger giriniz. ")
+    @NotBlank(message = "E-mail bilgisi bos gecilemez. Lutfen bir deger giriniz.")
     private String email;
+
+    @NotBlank(message = "Token bilgisi bos gecilemez. Lutfen bir deger giriniz. ")
     private String token;
 }
