3fd5a68f6f9c176761f1066c07c1dd45f642b325
Alperen İkinci <alperen.ikinci@hotmail.com>
2023-02-10 00:04:00 +0300

    CompanyManager update

--- company-director-service/src/main/java/com/bilgeadam/group1/service/CompanyManagerProfileService.java
+++ company-director-service/src/main/java/com/bilgeadam/group1/service/CompanyManagerProfileService.java
@@ -1,13 +1,17 @@
 package com.bilgeadam.group1.service;
 
+import com.bilgeadam.group1.dto.request.CompanyManagerProfileCreateRequestDto;
+import com.bilgeadam.group1.dto.request.ProfileUpdateRequest;
 import com.bilgeadam.group1.dto.request.RegisterRequestDto;
+import com.bilgeadam.group1.dto.request.UpdateTokenRequestDto;
+import com.bilgeadam.group1.dto.response.ProfileUpdateResponse;
 import com.bilgeadam.group1.dto.response.RegisterResponseDto;
 import com.bilgeadam.group1.dto.response.SummarisedFindAllResponse;
+import com.bilgeadam.group1.dto.response.UpdateTokenResponseDto;
 import com.bilgeadam.group1.exception.CompanyManagerException;
 import com.bilgeadam.group1.exception.ErrorType;
-import com.bilgeadam.group1.manager.IEmployeeManager;
 import com.bilgeadam.group1.mapper.ICompanyManagerProfileMapper;
-import com.bilgeadam.group1.repository.ICompanyProfileRepository;
+import com.bilgeadam.group1.repository.ICompanyManagerProfileRepository;
 import com.bilgeadam.group1.repository.entity.CompanyManagerProfile;
 import com.bilgeadam.group1.utility.CodeGenerator;
 import com.bilgeadam.group1.utility.ServiceManager;
@@ -15,44 +19,72 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyManagerProfileService extends ServiceManager<CompanyManagerProfile,Long> {
 
-    private final ICompanyProfileRepository companyProfileRepository;
+    private final ICompanyManagerProfileRepository companyManagerProfileRepository;
     private final ICompanyManagerProfileMapper companyManagerProfileMapper;
-    private final IEmployeeManager employeeManager;
 
-    public CompanyManagerProfileService(ICompanyProfileRepository companyProfileRepository, ICompanyManagerProfileMapper companyManagerProfileMapper, IEmployeeManager employeeManager){
-        super(companyProfileRepository);
-        this.companyProfileRepository = companyProfileRepository;
+    public CompanyManagerProfileService(ICompanyManagerProfileRepository companyManagerProfileRepository, ICompanyManagerProfileMapper companyManagerProfileMapper){
+        super(companyManagerProfileRepository);
+        this.companyManagerProfileRepository = companyManagerProfileRepository;
         this.companyManagerProfileMapper = companyManagerProfileMapper;
-        this.employeeManager = employeeManager;
     }
 
-    @Transactional
-    public RegisterResponseDto registerEmployee(RegisterRequestDto dto){
 
-        if (companyProfileRepository.findOptionalByName(dto.getEmail()).isPresent()){
-            throw  new CompanyManagerException(ErrorType.NAME_DUPLICATE);
-        }
-        CompanyManagerProfile companyManagerProfile= ICompanyManagerProfileMapper.INSTANCE.toCompanyManager(dto);
+    public boolean createCompanyManagerProfile (CompanyManagerProfileCreateRequestDto dto){
         try {
-            companyManagerProfile.setActivationCode(CodeGenerator.genarateCode());
-            save(companyManagerProfile);
-            employeeManager.createEmployee(ICompanyManagerProfileMapper.INSTANCE.toNewCreateEmployeeDto(companyManagerProfile));
-            return ICompanyManagerProfileMapper.INSTANCE.toRegisterResponseDto(companyManagerProfile);
+            CompanyManagerProfile profile = ICompanyManagerProfileMapper.INSTANCE.fromRequestToCompanyManagerProfile(dto);
+            companyManagerProfileRepository.save(profile);
+            return true;
         }catch (Exception e){
-            System.out.println(e.toString());
-            throw  new CompanyManagerException(ErrorType.EMPLOYEE_NOT_CREATED);
+            throw new CompanyManagerException(ErrorType.COMPANY_MANAGER_NOT_CREATED);
         }
     }
+
+    public Optional<CompanyManagerProfile> findByEmail(String email){
+        return companyManagerProfileRepository.findOptionalByEmail(email);
+    }
+
+    public Optional<UpdateTokenResponseDto> updateTokenByEmail(UpdateTokenRequestDto dto){
+        Optional<CompanyManagerProfile> profile = companyManagerProfileRepository.findOptionalByEmail(dto.getEmail());
+        if(!profile.isPresent()){
+            throw new CompanyManagerException(ErrorType.EMAIL_NOT_FOUND);
+        }
+        profile.get().setToken(dto.getToken());
+        companyManagerProfileRepository.save(profile.get());
+        return Optional.ofNullable(companyManagerProfileMapper.INSTANCE.fromTokenRequestToResponse(dto));
+    }
+
+    public Optional<CompanyManagerProfile> findOptionalByToken(String token){
+        return companyManagerProfileRepository.findOptionalByToken(token);
+    }
+
+    public Optional<ProfileUpdateResponse> updateProfileByToken(ProfileUpdateRequest request){
+        Optional<CompanyManagerProfile> profile = companyManagerProfileRepository.findOptionalByToken(request.getToken());
+        if(!profile.isPresent()){
+            throw new CompanyManagerException(ErrorType.INVALID_TOKEN);
+        }
+        profile.get().setName(request.getName());
+        profile.get().setPhotoUrl(request.getPhotoUrl());
+        profile.get().setMiddleName(request.getMiddleName());
+        profile.get().setSurname(request.getSurname());
+        profile.get().setPhone(request.getPhone());
+        profile.get().setAddress(request.getAddress());
+        profile.get().setUpdateDate(System.currentTimeMillis());
+        save(profile.get());
+        return Optional.ofNullable(companyManagerProfileMapper.INSTANCE.fromCompanyManagerProfileToProfileUpdateResponse(profile.get()));
+    }
+
+
     public List<CompanyManagerProfile> findAll(){
-        return companyProfileRepository.findAll();
+        return companyManagerProfileRepository.findAll();
     }
 
     public List<SummarisedFindAllResponse> findAllBySummarisedInformation(){
-        List<CompanyManagerProfile> responseList = companyProfileRepository.findAll();
+        List<CompanyManagerProfile> responseList = companyManagerProfileRepository.findAll();
         return ICompanyManagerProfileMapper.INSTANCE.fromCompanyManagerProfileToResponse(responseList);
     }
 }
